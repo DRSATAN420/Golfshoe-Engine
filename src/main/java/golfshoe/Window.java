d4b96a4d0a46ed3a24eebc344a3f59e1a7a4be2d
drsatan <brightdragon420@gmail.com>
2025-09-02 01:00:44 +0000

    init commit

--- src/main/java/golfshoe/Window.java
+++ src/main/java/golfshoe/Window.java
@@ -64,6 +64,10 @@ public class Window {
         return Window.window;
     }
 
+    public static Scene getScene(){
+        return get().currentScene;
+    }
+
     public void run(){
         System.out.println("hello LWJGL " + Version.getVersion() + "!");
 
